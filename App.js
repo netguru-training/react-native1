be0b410b5d8229289c4c0cf5a744edd6612d9281
Paweł Karniej <karniej.p@gmail.com>
2018-03-03 13:10:04 +0100

    added TaskFull

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TaskList from './src/components/TaskList'
+import TaskFull from './src/components/TaskFull'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <TaskList />
+        <TaskFull />
       </View>
     );
   }
