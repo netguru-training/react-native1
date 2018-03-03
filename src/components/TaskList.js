6c980b063ced2a94ec68a01418c42c4da5373842
Paweł Karniej <karniej.p@gmail.com>
2018-03-03 15:49:27 +0100

    passed editDescription as props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,29 @@
-import { View, FlatList, Text } from 'react-native';
-import React, { Component } from 'react';
-import TaskItem from './TaskItem'
-
+import { View, FlatList, Text } from "react-native";
+import React, { Component } from "react";
+import TaskItem from "./TaskItem";
 
 class TaskList extends Component {
-    renderCell = ({item}) => {
-      return (
-        <TaskItem item={item} checkItemWithId={this.props.checkItemWithId} editDescription={this.props.editDescription} />
-      )
-    }
-componentDidMount() {
-// this.props.checkItemWithId(0)
-}
-    render() {
-      
-        return (
-           <FlatList style={{flex: 1, paddingTop: 20}} data={this.props.data} renderItem={this.renderCell} />
-        );
-    }
+  renderCell = ({ item }) => {
+    return (
+      <TaskItem
+        item={item}
+        checkItemWithId={this.props.checkItemWithId}
+        editDescription={this.props.editDescription}
+      />
+    );
+  };
+  componentDidMount() {
+    // this.props.checkItemWithId(0)
+  }
+  render() {
+    return (
+      <FlatList
+        style={{ flex: 1, paddingTop: 20 }}
+        data={this.props.data}
+        renderItem={this.renderCell}
+      />
+    );
+  }
 }
 
 export default TaskList;
