15b7fbe8e077c47dd579a77aaca614ffc0167418
Natalia <bersien@Bears-MacBook-Pro.local>
2018-03-03 13:16:11 +0100

    added textinput and logic to add items

--- App.js
+++ App.js
@@ -1,39 +1,85 @@
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import TaskList from './src/components/TaskList'
+import { StyleSheet, Text, View, ListView, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
+
 
 export default class App extends React.Component {
+  state = {
+    items: [],
+    item: ''
+  }
+  ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+
   constructor() {
     super();
-    this.state = {
-        items: [
-            { title: ' ',
-            desc: ' ',
-            complete: false }
-        ]
-    };
-    this.deleteItem = this.deleteItem.bind(this);
+    AsyncStorage.getItem('items')
+      .then(itemsJSON => {
+        if(itemsJSON) {
+          this.setState({
+            items: JSON.parse(itemsJSON)
+          })
+        }
+      })
+  }
 
-    deleteItem(index) {
-        var items = this.state.items;
-        items.splice(index, 1);
-        this.setState({items: items})
-    }
+  onChangeText = text => {
+    this.setState({
+      item: text
+    })
+  }
+  onNewItem = e => {
+    const arr = [this.state.item, ...this.state.items];
+    this.setState({
+      items: arr,
+      item: ''
+    })
+    this.save(arr);
+  }
+
+  save = (arr) => {
+    AsyncStorage.setItem('items', JSON.stringify(arr))
+  }
+
+  renderRow = (rowData, sectionID, rowID) => {
+    return (
+      <View>
+        <Text>{rowData}</Text>
+        <TouchableOpacity
+          onPress={()=> {
+            this.state.items.splice(rowID, 1)
+            this.setState({
+              items: [...this.state.items]
+            })
+            this.save(this.state.items);
+          }}>
+          <Text>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
   render() {
+
     return (
-      <View style={styles.container}>
-        <TaskList />
+      <View>
+        <View>
+          <Text>TO DO LIST</Text>
+        </View>
+        <TextInput
+
+          onSubmitEditing={this.onNewItem}
+          placeholder='TODO: Title'
+          returnKeyType="done"
+          onChangeText={this.onChangeText}
+          value={this.state.item}
+        />
+
+        <ListView
+          dataSource={this.ds.cloneWithRows(this.state.items)}
+          renderRow={this.renderRow}
+          enableEmptySections
+          />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
