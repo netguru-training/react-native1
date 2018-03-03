e7aa3273346d335db2882003361c095e69cde7aa
Paweł Karniej <karniej.p@gmail.com>
2018-03-03 11:53:10 +0100

    third

---
+++ src/components/TaskList.js
@@ -0,0 +1,16 @@
+import { View, FlatList, Text } from 'react-native';
+import React, { Component } from 'react';
+import data from "./dummyData";
+
+
+class TaskList extends Component {
+    render() {
+        return (
+           <View>
+            <Text>Group 1</Text>
+           </View>
+        );
+    }
+}
+
+export default TaskList;
---
+++ src/components/dummyData.js
@@ -0,0 +1,22 @@
+export default {
+    {
+        "name": "do laundry",
+        "desc": " desc 1",
+        "isCompleted": false
+    },
+    {
+        "name": "do tings",
+        "desc": " desc 2",
+        "isCompleted": true
+    },
+    {
+        "name": "do elo",
+        "desc": " desc 3",
+        "isCompleted": true
+    },
+    {
+        "name": "do asdf",
+        "desc": " desc 3",
+        "isCompleted": false
+    }
+}
