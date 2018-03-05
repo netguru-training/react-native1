a3ec432b42ad03ad50641191670c3d3ff87c8d79
Patryk Mierzejewski <mierzej.pl@gmail.com>
2018-03-05 11:20:31 +0100

    FIxes Errors

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import MainNavigator from "./src/MainNavigator";
 export default class App extends React.Component {
+
+  state = {
+    isReady: false
+  }
+
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
     });
+    this.setState({isReady:true})
   }
 
   render() {
-    return <MainNavigator />;
+    if (this.state.isReady){
+      return <MainNavigator />;
+    } else {
+      return null
+    }
   }
 }
